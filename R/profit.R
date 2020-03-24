36ee080cd3724fde341297ca74aef71b5e2e5f36
P. Chen <shengnehs@qq.com>
2020-03-24 10:55:19 +0800

    1st

---
+++ R/profit.R
@@ -0,0 +1,16 @@
+#' total profit of industrial enterprises in China
+#'
+#' A dataset containing total profit of industrial enterprises in China.
+#'   Time span is from 2012-2 to 2019-12.
+#' @format  A data frame with 88 rows and 7 variables:
+#' \describe{
+#' \item{date}{date}
+#' \item{cumValue}{accumalative total profit, in current price RMB}
+#' \item{cumTB}{year-on-year ratio for accumalative total profit}
+#' \item{value}{total profit for the month, in current price RMB}
+#' \item{TB}{year-on-year ratio for total profit for the month}
+#' \item{yr}{year}
+#' \item{mon}{month}
+#' }
+#' @source Wind Database
+'profit'
---
+++ data-raw/profit.R
@@ -0,0 +1,12 @@
+## code to prepare `profit` dataset goes here
+rm(list = ls())
+library(tidyverse)
+profit <- openxlsx::read.xlsx('./data-raw/profit.xlsx',1,detectDates = T)
+profit <- profit[profit$指标名称 >= as.Date('2012-2-28'),]
+
+names(profit) <- c('date','cumValue','cumTB','value','TB')
+profit$yr <- format(profit$date,'%Y') %>% as.numeric()
+profit$mon <- format(profit$date,'%m') %>% as.numeric()
+
+profit[profit$mon == 2,'value'] <- profit[profit$mon == 2,'cumValue']
+# usethis::use_data(profit, overwrite = T)
